acce8256cfeeeba729d42dde1e8f85e0bc9b8f11
Jesus Saravia <jesusdsaravia@gmail.com>
2016-08-20 18:09:46 -0500

    interfaz ejercicio 4 terminada

--- src/intefaz/Ejercicio4.java
+++ src/intefaz/Ejercicio4.java
@@ -27,21 +27,87 @@ public class Ejercicio4 extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        txtMetros = new javax.swing.JTextField();
+        txtCuotai = new javax.swing.JTextField();
+        txtCuotaf = new javax.swing.JTextField();
+        cmdCalcular = new javax.swing.JButton();
+        cmdBorrar = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel1.setFont(new java.awt.Font("ST Gothic Hv", 2, 24)); // NOI18N
+        jLabel1.setText("Inmobilaria Saravia's Houses");
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 10, 360, 30));
+
+        jLabel2.setFont(new java.awt.Font("MS Reference Sans Serif", 0, 14)); // NOI18N
+        jLabel2.setText("Metros Cuadrados");
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 80, -1, -1));
+
+        jLabel3.setFont(new java.awt.Font("MS Reference Sans Serif", 0, 11)); // NOI18N
+        jLabel3.setText("Cuota inicial");
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 130, -1, -1));
+
+        jLabel4.setFont(new java.awt.Font("MS Reference Sans Serif", 0, 11)); // NOI18N
+        jLabel4.setText("Cuotas mensuales");
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 170, -1, -1));
+
+        txtMetros.setFont(new java.awt.Font("MS Reference Sans Serif", 0, 14)); // NOI18N
+        txtMetros.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtMetrosActionPerformed(evt);
+            }
+        });
+        jPanel1.add(txtMetros, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 80, 110, -1));
+
+        txtCuotai.setFont(new java.awt.Font("MS Reference Sans Serif", 0, 11)); // NOI18N
+        jPanel1.add(txtCuotai, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 130, 100, -1));
+
+        txtCuotaf.setFont(new java.awt.Font("MS Reference Sans Serif", 0, 11)); // NOI18N
+        txtCuotaf.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtCuotafActionPerformed(evt);
+            }
+        });
+        jPanel1.add(txtCuotaf, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 170, 100, -1));
+
+        cmdCalcular.setFont(new java.awt.Font("MS Reference Sans Serif", 0, 12)); // NOI18N
+        cmdCalcular.setForeground(new java.awt.Color(0, 102, 0));
+        cmdCalcular.setText("Calcular");
+        jPanel1.add(cmdCalcular, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 233, 100, 30));
+
+        cmdBorrar.setFont(new java.awt.Font("MS Reference Sans Serif", 0, 11)); // NOI18N
+        cmdBorrar.setForeground(new java.awt.Color(102, 0, 0));
+        cmdBorrar.setText("Borrar");
+        jPanel1.add(cmdBorrar, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 270, -1, -1));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void txtMetrosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtMetrosActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtMetrosActionPerformed
+
+    private void txtCuotafActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtCuotafActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtCuotafActionPerformed
 
     /**
      * @param args the command line arguments
@@ -79,5 +145,15 @@ public class Ejercicio4 extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton cmdBorrar;
+    private javax.swing.JButton cmdCalcular;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JTextField txtCuotaf;
+    private javax.swing.JTextField txtCuotai;
+    private javax.swing.JTextField txtMetros;
     // End of variables declaration//GEN-END:variables
 }
